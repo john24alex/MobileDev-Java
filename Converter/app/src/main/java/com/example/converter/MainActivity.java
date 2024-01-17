f9a0b102d37ba73fc3adaf2cb996769a367a63f9
John <john24alex@yahoo.com>
2024-01-17 15:07:37 -0500

    Project starter

---
+++ Converter/app/src/main/java/com/example/converter/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.accessibility.AccessibilityViewCommand;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Convert(View view) {
+        EditText editText=findViewById(R.id.editTextNumber2);
+        TextView textView=findViewById(R.id.textView);
+        String dollar = editText.getText().toString();
+
+        if (!dollar.equals("")) {
+            Float euro = Float.valueOf(dollar)*0.85f;
+textView.setText(euro.toString());
+
+        }else{
+            textView.setText("You didn't enter any value");
+        }
+
+
+    }
+}
